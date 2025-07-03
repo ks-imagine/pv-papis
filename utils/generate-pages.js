2d028b503bf103b3054333dc82064874ec9f5240
Kevin Scaringi <kscaringi@microsoft.com>
2025-07-02 23:45:06 -0700

    Add palm tree emoji favicon � to all pages

--- utils/generate-pages.js
+++ utils/generate-pages.js
@@ -30,6 +30,7 @@ function generateQuestionPage(team, questionNumber, question) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${teamName} - Question ${questionNumber}</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌴</text></svg>">
     <link rel="stylesheet" href="../../styles/shared-styles.css">
 </head>
 <body class="${team}-theme">
